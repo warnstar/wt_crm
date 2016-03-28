a54b29c7d3cca423f592da2d586772977c0887f5
wchuang <wchuang@aliyun.com>
2016-03-28 14:18:48 +0800

    微信调试

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -31,6 +31,19 @@ class SiteController extends Controller
             ],
         ];
     }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
     public function actionExtra_login(){
 
         $get = Yii::$app->request->get();
@@ -61,7 +74,7 @@ class SiteController extends Controller
         if($user_info){
             //授权通过，获得用户信息
             dump($user_info);
-            
+
 
 
         }else{
